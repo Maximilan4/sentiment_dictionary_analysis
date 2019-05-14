4d5bf39cf9e9d1d942abe1652b4acefc7660a56d
m.maximov <psycho@rft.email>
2019-05-14 21:10:42 +0400

    Analyse of single phrase, readme

---
+++ readme.md
@@ -0,0 +1,12 @@
+## Требования
+- python 3.6 или выше
+
+## Инициализация
+1) клонировать или скачать проект
+2) перейти в директорию проекта через терминал
+3) выполнить `pip3 install -r requirements.txt`
+4) выполнить `python3 -m download.py`
+5) выполнить `cat config_example.py > config.py` или скопировать ручками содержимое `config_example.py` в `config.py`
+
+## Запуск
+`python3 -m run.py`
--- src/command.py
+++ src/command.py
@@ -1,6 +1,8 @@
 import config
+import sys
+
 from src.sentiment import SentimentAnalysis
-import nltk
+
 
 
 class App:
@@ -9,7 +11,35 @@ class App:
         self.dictionary_path = config.DICTIONARY_PATH
         self.analyser = SentimentAnalysis(config.DICTIONARY_PATH)
 
-    def run(self):
-        result = self.analyser.score("абракадавра")
-        print(result)
+    def routes(self):
+        return (
+            self.analyse_single,
+            self.analyse_file
+        )
 
+    def analyse_single(self):
+        text = input("Print your text (en) here : ")
+        result = self.analyser.score(text)
+
+        formatted_output = "The result from -1 to 1 is : {}".format(result)
+        print(formatted_output)
+        print("\n")
+        self.run()
+
+    def analyse_file(self):
+        print("future file analysis")
+
+    def run(self):
+        print("That do you suppose to do?")
+        print("[0] - input single phrase")
+        print("[1] - analyse file")
+        print("[:q] - exit")
+
+        action = None
+        while action not in ('0', '1'):
+            action = input("Select option: ")
+            if action in ('q', ':q', 'quit'):
+                sys.exit(1)
+
+        routes = self.routes()
+        routes[int(action)]()
